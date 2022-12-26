0f8a9875e6ab38e1976a15c661c05d1922b98d99
trkyshorty <trky.shorty@gmail.com>
2022-12-26 12:03:39 +0300

    Update rank.js

--- src/commands/level/rank.js
+++ src/commands/level/rank.js
@@ -64,7 +64,7 @@ class Rank extends Command {
             guildMemberDocs[userPosition].level * 121,
             defaultColor
           )
-          .setStatus(guildMember.presence.status)
+          .setStatus(guildMember.presence.status ? guildMember.presence.status : 'offline')
           .setProgressBar(progressBarColor, 'COLOR')
           .setRank(userPosition + 1, '#')
           .setRankColor(defaultColor, defaultColor)
